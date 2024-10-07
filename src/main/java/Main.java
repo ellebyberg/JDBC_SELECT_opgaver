299ff773a05522e394e231f0b240a0e0cd26fd65
martinellebyberg <mabe0003@kea.stud.dk>
2024-10-07 18:21:05 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+       EmployeeRepository employeeRepository = new EmployeeRepository();
+
+        //employeeRepository.printEmployeeNumberAndName();
+        //employeeRepository.printEmployeeNameStartsWithS();
+        //employeeRepository.printAllExpenses();
+        //employeeRepository.printHighestPaidEmployee();
+        //employeeRepository.printNumberOfEmployeesWhoEarnsMoreThanAverage();
+        //mployeeRepository.printEmployeesBasedOnManager(7521); VIRKER IKKE
+    }
+}
